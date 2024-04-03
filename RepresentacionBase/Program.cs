6f9df314dedefcbfa0a9519e64184309411f6e33
Pablo Rúa <pablo_rua182@hotmail.com>
2024-04-03 13:09:49 -0500

    Xtra comments

--- RepresentacionBase/Program.cs
+++ RepresentacionBase/Program.cs
@@ -4,8 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ejemplo de uso del metodo ConvertBase
         int NumX = 287;
         int baseK = 4;
+
         string RepBase = ConvertBase(NumX, baseK);
         Console.WriteLine($"El numero {NumX} en base {baseK} es: {RepBase}");
     }
@@ -14,11 +16,12 @@ class Program
     {
         string Resultado = "";
 
+        //Conidicional para evitar cantidad 0.
         if(NumX == 0)
         {
             return "0";
         }
-
+        //Bucle para dividir la base por la cantidad.
         while(NumX > 0)
         {
             int Residuo = NumX % baseK;
